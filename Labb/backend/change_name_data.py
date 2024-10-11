72193b769cd4cc1f23375483670d20b3d02203d2
Pontus Berglund <pontusberglund@hotmail.com>
2024-10-11 19:12:48 +0200

    Before lab

--- Labb/backend/change_name_data.py
+++ Labb/backend/change_name_data.py
@@ -12,4 +12,3 @@ cleaned_data_path.mkdir(parents=True, exist_ok=True)
 for folder in raw_data_path.iterdir():
     new_name = folder.name.split()[0]
     copytree(folder, cleaned_data_path / new_name)
-
---
+++ code_alongs/09_Python_connect/constants.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+DATABASE_PATH = Path(__file__).parent/"göteborg_stad.db"
+DATA_PATH = Path(__file__).parent / "data"
